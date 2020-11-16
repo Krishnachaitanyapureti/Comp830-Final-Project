097a66b2d426e7ffc41f5326aef512d4367b9165
Krishnachaitanyapureti <unhmguest@UNHM-128-01.ad.unh.edu>
2020-11-16 18:49:28 -0500

    'week1'

--- Model.java
+++ Model.java
@@ -3,11 +3,8 @@ package pacman;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import javax.swing.ImageIcon;
+
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
 public class Model extends JPanel implements ActionListener {
 
